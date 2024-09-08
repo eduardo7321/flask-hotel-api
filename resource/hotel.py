a1bfd7d0b0413c90bf187d50106ffab3a018ec31
eduardo <eduardo.jose184@gmail.com>
2024-09-07 21:23:51 -0300

    Implementando HotelModel em models

--- resource/hotel.py
+++ resource/hotel.py
@@ -1,5 +1,7 @@
 from flask_restful import Resource, reqparse
 
+from models.hotel import HotelModel
+
 hoteis = [
     {
         "hotel_id": 1,
@@ -55,21 +57,17 @@ class Hotel(Resource):
             return hotel
         return {'message': 'Hotel not found.'}, 404 # not found
 
-    def post(self, hotel_id):       
-
+    def post(self, hotel_id):
         dados = Hotel.argumentos.parse_args()
-
-        novo_hotel = {'hotel_id': hotel_id, **dados}
-
+        hotel_objeto = HotelModel(hotel_id, **dados)
+        novo_hotel = hotel_objeto.json()
         hoteis.append(novo_hotel)
         return novo_hotel, 200
 
     def put(self, hotel_id):
-
         dados = Hotel.argumentos.parse_args()
-
-        novo_hotel = {'hotel_id': hotel_id, **dados}
-
+        hotel_objeto = HotelModel(hotel_id, **dados)
+        novo_hotel = hotel_objeto.json()
         hotel = Hotel.find_hotel(hotel_id)
         if hotel:
             hotel.update(novo_hotel)
